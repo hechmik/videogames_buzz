c69ff5102384b7c83e52d06014f14c324f33f277
hechmik <hechmi.khaled1995@gmail.com>
2019-04-10 14:50:04 +0200

    Basic script for scraping API endpoints

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+def store_results(web_page, file_name):
+    content = requests.get(web_page).json()
+    with open(file_name, 'w') as outfile:
+        json.dump(content, outfile, indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+
+    steam_spy_all = "http://steamspy.com/api.php?request=all"
+    filename = "steam_spy_all.json"
+    store_results(steam_spy_all, filename)
