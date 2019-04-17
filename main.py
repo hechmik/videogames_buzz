2bf4dc3c7893d6b5b9b409a44945e121867dfd03
hechmik <hechmi.khaled1995@gmail.com>
2019-04-17 16:51:31 +0200

    add request to a specific app

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ if __name__ == "__main__":
     steam_spy_all = "http://steamspy.com/api.php?request=all"
     filename = "steam_spy_all.json"
     store_results(steam_spy_all, filename)
+
+    single_app_detail = "http://steamspy.com/api.php?request=appdetails&appid=730"
+    filename = "appid_730.json"
+    store_results(single_app_detail, filename)
